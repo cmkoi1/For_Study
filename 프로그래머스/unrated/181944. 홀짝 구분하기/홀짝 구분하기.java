11f5f1952a4231811665b3eec393f9d92cdc0034
cmkoi1 <103043037+cmkoi1@users.noreply.github.com>
2023-09-13 17:20:26 +0900

    [unrated] Title: 홀짝 구분하기, Time: 157.05 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n%2==1){
+            System.out.printf("%d is odd", n);
+        } else {
+            System.out.printf("%d is even", n);
+        }
+    }
+}
