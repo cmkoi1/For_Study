57d52a2a0fe1f2f0eadec07d60793b2bbb54ee62
김지민 <103043037+cmkoi1@users.noreply.github.com>
2025-03-12 22:19:36 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int one = 0;
+        int two = 0;
+        
+        for(int i = 0; i<num_list.length; i++){
+            if(i % 2 == 0) {
+                one += num_list[i];
+            } else {
+                two += num_list[i];
+            }
+        }
+        
+        return Math.max(one, two);
+    }
+}
